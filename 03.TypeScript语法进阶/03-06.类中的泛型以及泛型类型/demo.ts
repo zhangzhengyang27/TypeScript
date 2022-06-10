53e5786ce9ef56abb7f6df052e8654f3ce21999b
zhangzhengyang <1074385735@qq.com>
2022-06-10 10:45:35 +0800

    ts类中泛型以及泛型类型

--- 03.TypeScript语法进阶/03-06.类中的泛型以及泛型类型/demo.ts
+++ 03.TypeScript语法进阶/03-06.类中的泛型以及泛型类型/demo.ts
@@ -1,32 +1,45 @@
-// interface Item {
-//   name: string;
-// }
+interface Item {
+    name: string;
+}
 
-// class DataManager<T extends number | string> {
-//   constructor(private data: T[]) {}
-//   getItem(index: number): T {
-//     return this.data[index];
-//   }
-// }
+class DataManager<T extends number | string> {
+    constructor(private data: T[]) {
+    }
 
-// const data = new DataManager<number>([1]);
-// data.getItem(0);
+    getItem(index: number): T {
+        return this.data[index];
+    }
+}
 
-// const data = new DataManager([
-//   {
-//     name: 'dell'
-//   }
-// ]);
+const data = new DataManager<number>([1]);
+data.getItem(0);
 
-// interface Test {
-//   name: string;
-// }
+// 泛型继承接口
+class DataManager1<T extends Item> {
+    constructor(private data: T[]) {
+    }
 
-// const data = new DataManager<number>([]);
+    getItem(index: number): T {
+        return this.data[index];
+    }
+}
+
+const data1 = new DataManager1([
+    {
+        name: 'dell'
+    },
+    {
+        name: 'zhang'
+    }
+]);
+data1.getItem(0);
+
 
 // 如何使用泛型作为一个具体的类型注解
 function hello<T>(params: T) {
-  return params;
+    return params;
 }
 
 const func: <T>(param: T) => T = hello;
+
+const func1: <T>(param: T) => T = <T>(params: T) => params;
