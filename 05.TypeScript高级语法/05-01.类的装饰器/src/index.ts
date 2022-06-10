d3c062c5369632128bc940fa17a9a5b76adc909e
zhangzhengyang <1074385735@qq.com>
2022-06-10 14:34:30 +0800

    类的装饰器

--- 05.TypeScript高级语法/05-01.类的装饰器/src/index.ts
+++ 05.TypeScript高级语法/05-01.类的装饰器/src/index.ts
@@ -1,22 +1,26 @@
 function testDecorator() {
-  return function<T extends new (...args: any[]) => any>(constructor: T) {
-    return class extends constructor {
-      name = 'lee';
-      getName() {
-        return this.name;
-      }
+    // (...args: any[]) => any 是一个函数，返回值是any类型的
+    return function <T extends new (...args: any[]) => any>(constructor: T) {
+        return class extends constructor {
+            name = 'lee';
+
+            getName() {
+                return this.name;
+            }
+        };
     };
-  };
 }
 
+// testDecorator()(class{})  里面的函数class{} 是一个参数
 const Test = testDecorator()(
-  class {
-    name: string;
-    constructor(name: string) {
-      this.name = name;
+    class {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
     }
-  }
 );
 
 const test = new Test('dell');
-console.log(test.getName());
+console.log(test.getName());  // dell
--- 05.TypeScript高级语法/05-01.类的装饰器/src/simple_decorator.ts
+++ 05.TypeScript高级语法/05-01.类的装饰器/src/simple_decorator.ts
@@ -1,22 +1,24 @@
-// 类的装饰器
+// 类的装饰器，对类进行装饰，而不是实例
 // 装饰器本身是一个函数
 // 类装饰器接受的参数是构造函数
 // 装饰器通过 @ 符号来使用
 
-// function testDecorator(flag: boolean) {
-//   if (flag) {
-//     return function(constructor: any) {
-//       constructor.prototype.getName = () => {
-//         console.log('dell');
-//       };
-//     };
-//   } else {
-//     return function(constructor: any) {};
-//   }
-// }
+function ZhangtestDecorator(flag: boolean) {
+    if (flag) {
+        return function (constructor: any) {
+            constructor.prototype.getName = () => {
+                console.log('dell');
+            };
+        };
+    } else {
+        return function (constructor: any) {
+        };
+    }
+}
 
-// @testDecorator(true)
-// class Test {}
+@ZhangtestDecorator(true)
+class Test1 {
+}
 
-// const test = new Test();
-// (test as any).getName();
+const testDemo = new Test1();
+(testDemo as any).getName();
