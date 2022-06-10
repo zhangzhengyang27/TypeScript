ea7211e42654a1ecd2ea0f71d750e207be7cbc01
zhangzhengyang <1074385735@qq.com>
2022-06-10 15:00:00 +0800

    方法装饰器

--- 05.TypeScript高级语法/05-02.方法装饰器/src/index.ts
+++ 05.TypeScript高级语法/05-02.方法装饰器/src/index.ts
@@ -2,22 +2,28 @@
 // 静态方法，target 对应的是类的构造函数
 
 function getNameDecorator(target: any, key: string, descriptor: PropertyDescriptor) {
-  // console.log(target, key);
-  // descriptor.writable = true;
-  descriptor.value = function() {
-    return 'decorator';
-  };
+    console.log(target, key);
+    // { getName: [Function (anonymous)] } getName
+
+    // 方法不可以被修改
+    // descriptor.writable = true;
+    // descriptor.value = function () {
+    //     return 'decorator';
+    // };
 }
 
+// 在类的方法增加之后就对方法进行装饰
 class Test {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-  }
-  @getNameDecorator
-  getName() {
-    return this.name;
-  }
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    @getNameDecorator
+    getName() {
+        return this.name;
+    }
 }
 
 const test = new Test('dell');
