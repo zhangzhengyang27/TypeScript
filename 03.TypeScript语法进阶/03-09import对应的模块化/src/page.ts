c1ec1d208ec6f4ac1b07c6e4172a7dee31fafe12
zhangzhengyang <1074385735@qq.com>
2022-06-10 11:08:57 +0800

    ts import 的引入语法

---
+++ 03.TypeScript语法进阶/03-09import对应的模块化/src/page.ts
@@ -0,0 +1,12 @@
+import {Header, Content, Footer} from './components';
+
+
+export class Page {
+    constructor() {
+        new Header();
+        new Content();
+        new Footer();
+        new Footer();
+    }
+}
+
